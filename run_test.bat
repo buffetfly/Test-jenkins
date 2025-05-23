92a45b482e875411290750201880609ebc331cfd
MaiQiFeng <1192437392@qq.com>
2025-05-23 11:28:26 +0800

    上传测试bat脚本

--- run_test.bat
+++ run_test.bat
@@ -1,5 +1,24 @@
+@echo off
 echo Running unit tests...
-D:\project\Test-jenkins\Test_jenkins\test_static_Release\Test_jenkins_test.exe -xml -o D:/project/Test-jenkins/test_result.xml
+setlocal enabledelayedexpansion
+REM === 参数解析（从 Jenkins 传入 GIT_BRANCH，例如 origin/test） ===
+set FULL_BRANCH=%1
+for /f "tokens=2 delims=/" %%i in ("%FULL_BRANCH%") do (
+    set BRANCH_NAME=%%i
+)
+
+REM 如果未传入参数，则尝试用 git 获取（本地手动运行时用）
+if not defined BRANCH_NAME (
+    for /f %%i in ('git rev-parse --abbrev-ref HEAD') do (
+        set BRANCH_NAME=%%i
+    )
+)
+
+
+set OUTPUT_DIR=D:\project\Test-jenkins\Test_jenkins\%BRANCH_NAME%_Release
+set BUILD_DIR=%OUTPUT_DIR%
+
+D:\project\Test-jenkins\Test_jenkins\test_static_Release\Test_jenkins_test.exe -xml -o %BUILD_DIR%/test_result.xml
 IF %ERRORLEVEL% NEQ 0 (
     echo  Unit tests failed.
     pause >nul
