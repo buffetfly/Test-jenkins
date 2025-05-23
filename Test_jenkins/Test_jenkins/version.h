06dc54b721690e80a137aa1b02824d20983da7ab
MaiQiFeng <1192437392@qq.com>
2025-05-23 17:40:48 +0800

    提交

--- Test_jenkins/Test_jenkins/version.h
+++ Test_jenkins/Test_jenkins/version.h
@@ -1,8 +1,8 @@
 #define FILE_VER_MAJOR 1 
 #define FILE_VER_MINOR 0 
-#define FILE_VER_PATCH 75 
-#define BUILD_NUM 0278524ad78a1eeda6b7097a8fd5d781cac7b0a9 
-#define FILE_VER_STR "75" 
-#define PRODUCT_VER_STR "75-0278524ad78a1eeda6b7097a8fd5d781cac7b0a9" 
+#define FILE_VER_PATCH 76 
+#define BUILD_NUM 9ad8a1f80d684a62bcc6610a21f3cea948f1f9b1 
+#define FILE_VER_STR "76" 
+#define PRODUCT_VER_STR "76-9ad8a1f80d684a62bcc6610a21f3cea948f1f9b1" 
 #define FILE_DESC "Test_jenkins test_static" 
  
